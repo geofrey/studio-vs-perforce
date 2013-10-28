0bc299d72eb06e39c1d024e91e3305fe28c3515e
U-CPIC-CORP\gsanders <gsanders@WL-9X406S1.corporate.citizensfla.com>
2013-10-28 11:43:13 -0400

    trying to teach p4shim to log in when P4V's session has expired

--- p4shim.py
+++ p4shim.py
@@ -1,17 +1,20 @@
 import datetime
+import io
 import os
 import re
 import subprocess
 import sys
+import time
 
 args = sys.argv[1:]
 
 def extricate(flag, envname):
 	global args
-	p_at = args.index(flag)
-	value = args[p_at+1]
-	args[p_at:p_at+2] = []
-	os.putenv(envname, value)
+	if flag in args:
+		p_at = args.index(flag)
+		value = args[p_at+1]
+		args[p_at:p_at+2] = []
+		os.putenv(envname, value)
 
 # It may be peculiar to my setup, but p4 is ignoring the username and password options and only responds to the corresponding environment variables.
 to_env = [('-u', 'P4USER'), ('-P', 'P4PASSWD')]
@@ -42,6 +45,20 @@ commandline = ' '.join([p4]+args)
 log = open(r'c:\dev\tools\studio-vs-perforce\p4shim.txt', 'a')
 stamp = datetime.datetime.now()
 log.write('{}\t{} {}\n'.format(stamp, p4, ' '.join(args)))
+
+last = os.stat('p4shim.flag').st_atime
+if last < time.time() - 9 * 60 * 60:
+	localpipe = io.StringIO()
+	subprocess.call('p4 tickets', stdout=localpipe) # see if there are any active logins
+	localpipe.seek(0)
+	if len(localpipe.readlines()) == 0:
+		log.write('[Sign in first.]')
+		#username = open('p4username.txt').readline().strip()
+		#subprocess.call('p4 login {}'.format(username), shell=True, stdin=open('p4password.txt'))
+		subprocess.call('p4 login', shell=True, stdin=open('p4password.txt'), stdout=log, stderr=log) # complains if you specify a username, would rather assume you mean the logged-in user. Weird.
+		os.utime('p4shim.flag')
+
+
 log.flush()
 sys.stdout.flush()
 subprocess.call(commandline, shell=True, stderr=log)
