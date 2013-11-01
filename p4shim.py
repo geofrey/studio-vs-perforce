82fac485326f615db0bbe0ba18b2887f5edaef65
Geofrey Sanders <geofrey.sanders@gmail.com>
2013-11-01 15:15:57 -0400

    Check logged-in status with 'p4 login -s' instead of counting tickets.

--- p4shim.py
+++ p4shim.py
@@ -23,15 +23,14 @@ def extricate(flag, envname):
 to_env = [('-u', 'P4USER'), ('-P', 'P4PASSWD')]
 list(map(lambda params: extricate(*params), to_env)) # list() to force evaluation - map is lazy
 
-# if there's no active ticket, log in before proceeding
-tickets = subprocess.Popen(['p4', 'tickets'], stdout=subprocess.PIPE, stderr=log).communicate()[0].split()
-if len(tickets) == 0:
+if 'expires in' not in str(subprocess.Popen(['p4', 'login', '-s'], stdout=subprocess.PIPE, stderr=log).communicate()[0]):
 	log.write('[Log in.]\n')
 	thepass = r'C:\dev\tools\studio-vs-perforce\p4shim.password'
 	status = subprocess.call('p4 login', stdin=open(thepass), stdout=open(os.devnull), stderr=log)
 	if status != 0:
 		log.write('[Login error]\n')
 		# maybe quit here?
+
 def tokenize_args(args):
 	args2 = args[:]
 	while args2:
